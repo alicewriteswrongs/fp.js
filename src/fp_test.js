198a1c39c02a7aa09db79e6cfde8de5a232a6984
Alice Pote <alice.writes.wrongs@gmail.com>
2016-10-10 15:42:39 -0400

    starting to do testing

--- src/fp_test.js
+++ src/fp_test.js
@@ -1,9 +1,26 @@
 import {
+  curry,
+  compose,
   cons,
   flatten,
 } from './fp';
 
 describe("alice's little functional programming library", () => {
+  describe('curry', () => {
+    const timesTwo = x => x * 2
+    const plusTwo = x => x + 2
+    const plusFour = x => x + 4
+
+    it('should chain functions together', () => {
+      let combined = compose(timesTwo, plusTwo)
+
+      assert.equal(combined(2), 8);
+    });
+  });
+
+  describe('compose', () => {
+  });
+
   describe('cons', () => {
     it('should build arrays from an element and an array', () => {
       cons('a', ['b', 'c'])
