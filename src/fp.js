d0cfc787132bf9ada816f2ca4345ea91748fa53e
Alice Pote <alice.writes.wrongs@gmail.com>
2016-11-02 10:08:32 -0400

    new file for string

--- src/fp.js
+++ src/fp.js
@@ -1,22 +1,3 @@
-export const strcpy = s => s.concat()
-
-export const toLower = s => s.toLowerCase()
-
-export const toUpper = s => s.toUpperCase()
-
-export const _firstCap = s => s.replace(/^./, toUpper(head(s)))
-
-export const capitalize = compose(_firstCap, toLower, strcpy)
-
-export const split = curry((sep, s) => s.split(sep))
-
-export const join = curry((char, s) => s.join(char))
-
-export const words = split(' ')
-
-export const sentence = join(' ')
-
-export const capitalizeSentence = compose(sentence, editHead(capitalize), words)
 
 // object methods
 export const fromPairs = xs => (
